dea2a3cbf11659f8ee07db32bb49951c017a593e
Isaac <isaac.patrickson@mediaworks.co.uk>
2024-10-25 11:35:30 +0100

    Added a dark mode. Can be toggled by a button on the page. Enqued the listener for the button and created a separate css file for buttons

--- functions.php
+++ functions.php
@@ -1,5 +1,16 @@
 <?php
 function enqueue_theme_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_uri());
+
+    wp_enqueue_style( 
+		'theme-button-style',
+		get_parent_theme_file_uri( 'assets/css/button.css' )
+	);
 }
+
+function enqueue_theme_scripts() {
+    wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), null, true);
+}
+
 add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
+add_action('wp_enqueue_scripts', 'enqueue_theme_scripts');
