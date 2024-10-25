5ba599b504f6d3e93829249db8aac452ce41a4c4
Isaac <isaac.patrickson@mediaworks.co.uk>
2024-10-25 14:33:34 +0100

    Reverted back. Abandonned the dark mode toggle.

--- functions.php
+++ functions.php
@@ -8,9 +8,4 @@ function enqueue_theme_styles() {
 	);
 }
 
-function enqueue_theme_scripts() {
-    wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), null, true);
-}
-
 add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
-add_action('wp_enqueue_scripts', 'enqueue_theme_scripts');
